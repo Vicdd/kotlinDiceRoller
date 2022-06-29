9119981a3398df9f148f506a2d0e27946c7e1769
VictorCamolesi <victorrobcam@gmail.com>
2022-06-29 16:59:15 -0300

    debugging

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
+
 
 class Dice(val numSides: Int) {
     fun roll(): Int {
@@ -48,6 +52,27 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
         rollDice()
-
+        logging()
+        division()
     }
+
+    private fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+
+    private fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Log.d(TAG, "$denominator")
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+
+
 }
